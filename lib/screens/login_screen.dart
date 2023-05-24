040210893bdbfac26aee4851fe2bf8ad0be1e609
Xaiodo <rostislavgovbak@gmail.com>
2023-05-24 11:46:27 +0300

    Completed login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../values/app_colors.dart';
+import '../widgets/eleveted_button.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Login'),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          padding:
+              EdgeInsets.only(top: MediaQuery.of(context).size.width / 3.77),
+          alignment: Alignment.center,
+          decoration: const BoxDecoration(
+            gradient: AppColors.backgroundGradient,
+          ),
+          child: MyElevatedButton(
+            title: "Вхід",
+            backgroundColor: AppColors.primaryVariant,
+            foregroundColor: Colors.black,
+            onPressed: () => context.go('/home'),
+            padding: const EdgeInsets.symmetric(horizontal: 46, vertical: 10),
+          ),
+        ),
+      );
 }
