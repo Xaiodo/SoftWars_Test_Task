1527efa9677239eae8ee8cc31c7ed4b415309f1e
Xaiodo <rostislavgovbak@gmail.com>
2023-05-24 21:10:25 +0300

    Created tasks service.

---
+++ lib/services/tasks_service.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import '../values/api.dart';
+import '../models/task.dart';
+
+class TasksService {
+  final String baseUrl = Api.baseUrl;
+  final Dio _dio;
+
+  TasksService({required Dio dio}) : _dio = dio;
+
+  Future<List<Task>> getTasks() async {
+    try {
+      final response = await _dio.get('${baseUrl}tasks');
+      final tasks = response.data['data']
+          .map<Task>((task) => Task.fromJson(task))
+          .toList();
+      return tasks;
+    } on DioError {
+      rethrow;
+    }
+  }
+}
