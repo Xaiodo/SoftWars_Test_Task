5c84ed2db1b2fb631d4c30f901a7faa36976dbb5
Xaiodo <rostislavgovbak@gmail.com>
2023-05-24 11:25:23 +0300

    Created theme and colors settings

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const LinearGradient backgroundGradient = LinearGradient(
+    colors: [
+      Color(0xFFA9A9A9),
+      Color(0xFF383838),
+    ],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+  static const Color primary = Color(0xFFDBDBDB);
+  static const Color primaryVariant = Color(0xFFFFD600);
+  static const Color disabled = Color(0xFFFBEFB4);
+  static const Color accentRed = Color(0xFFFF8989);
+  static const Color secondary = Color(0xFF383838);
+}
---
+++ lib/values/theme.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+const appBarTheme = AppBarTheme(
+  foregroundColor: AppColors.secondary,
+  elevation: 0,
+  iconTheme: IconThemeData(
+    color: AppColors.primaryVariant,
+  ),
+);
+
+const actionFloatingButtonTheme = FloatingActionButtonThemeData(
+  backgroundColor: AppColors.primaryVariant,
+  foregroundColor: AppColors.secondary,
+);
+
+const textTheme = TextTheme(
+  titleLarge: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: AppColors.secondary,
+  ),
+  titleMedium: TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: AppColors.secondary,
+  ),
+  titleSmall: TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.secondary,
+  ),
+  bodySmall: TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+  ),
+);
+
+final theme = ThemeData(
+  primarySwatch: Colors.blue,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
